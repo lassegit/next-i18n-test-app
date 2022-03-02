e958e563fa542ecd5e2c5561c8d5e50dac1745a9
Lasse <lassetange@gmail.com>
2022-03-02 16:52:37 +0100

    Testing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,24 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { IntlProvider } from 'react-intl';
 import { useRouter } from 'next/router';
-import de from '../locales/compiled/de.json';
+// import de from '../locales/compiled/de.json';
+import i18nconfig from '../git18n.config.json';
+import { getMessages } from 'git18n';
+
+const messages: {} = i18nconfig.translations.map((t) => {
+  try {
+    return { [t]: require(`../node_modules/git18n/locales/${t}.json`) };
+  } catch (error) {
+    return { [t]: {} };
+  }
+});
+
+// import {sum} from "git18n";
+// const messages = getMessages("de,fr,es,it");
+
+// const TRANSLATIONS = ['de_at', 'de', 'da'];
 
 const DEFAULT_LOCALE = 'en';
-const TRANSLATIONS = ['de-at', 'de', 'da'];
-
 const getLocale = (locale?: string) => {
   if (locale && locale.startsWith('de')) {
     return 'de';
@@ -18,10 +31,10 @@ const getLocale = (locale?: string) => {
 function MyApp({ Component, pageProps }: AppProps) {
   const { locale } = useRouter();
   const localeToUse = getLocale(locale);
-  const messages = { de, en: {} };
+  const messages = getMessages();
 
   return (
-    <IntlProvider locale={localeToUse} defaultLocale={DEFAULT_LOCALE} messages={de}>
+    <IntlProvider locale={localeToUse} defaultLocale={i18nconfig.defaultLanguage} messages={messages}>
       <Component {...pageProps} />
     </IntlProvider>
   );
