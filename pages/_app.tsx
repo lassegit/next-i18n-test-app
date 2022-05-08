971869e8b259c2dcf04477ef8024779bc510cb90
Lasse <lassetange@gmail.com>
2022-05-08 19:32:02 +0200

    Update locales

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,22 +4,17 @@ import { IntlProvider } from 'react-intl';
 import { useRouter } from 'next/router';
 
 const defaultLocale = 'en';
-const locales = ['de', 'dk'];
+const locales = ['de', 'dk', 'br'];
 
 const messages = locales.reduce<{ [key: string]: {} }>((acc, cur) => {
   try {
-    console.log(cur);
-    acc[cur] = require(`../node_modules/git18n/locales/${cur}.json`);
-  } catch (err) {
-    console.log(err);
-  }
+    acc[cur] = require(`../.locales/${cur}.json`);
+  } catch {}
   return acc;
 }, {});
 
-console.log({ messages });
-
 function MyApp({ Component, pageProps }: AppProps) {
-  const { locale = 'en' } = useRouter();
+  const { locale = 'de' } = useRouter();
 
   return (
     <IntlProvider locale={locale} defaultLocale={defaultLocale} messages={messages[locale]}>
